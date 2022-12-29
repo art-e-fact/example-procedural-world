d8ffa6c122c6ced665639a68054180477cc678fe
Polgár András <azazdeaz@gmail.com>
2022-12-29 17:05:05 +0100

    Implement basic a-star planning

--- ros2_ws/src/nav_test/nav_test/robot.py
+++ ros2_ws/src/nav_test/nav_test/robot.py
@@ -1,12 +1,13 @@
-from geometry_msgs.msg import Twist, Point, TransformStamped, Pose
+from geometry_msgs.msg import Twist, Point, TransformStamped, Pose, PoseStamped
 from tf2_msgs.msg import TFMessage
 from tf2_geometry_msgs import do_transform_pose
 from visualization_msgs.msg import Marker
 from std_msgs.msg import ColorRGBA
 from sensor_msgs.msg import LaserScan
-from nav_msgs.msg import OccupancyGrid, MapMetaData
+from nav_msgs.msg import OccupancyGrid, MapMetaData, Path
 
 import numpy as np
+import cv2
 
 import rclpy
 import rclpy.time
@@ -41,7 +42,7 @@ class Robot(Node):
         )
         # Set the default map resolution to 20cm
         self.map_resolution = float(
-            self.declare_parameter("map_resolution", "0.2")
+            self.declare_parameter("map_resolution", "0.1")
             .get_parameter_value()
             .string_value
         )
@@ -69,11 +70,18 @@ class Robot(Node):
         )
         self.publisher_cmd_vel = self.create_publisher(Twist, "cmd_vel", 1)
         self.publisher_map = self.create_publisher(OccupancyGrid, "map", 1)
+        self.publisher_path = self.create_publisher(Path, "path", 1)
 
         # Calculate and send new navigation commands periodically
         self.timer = self.create_timer(1.0, self.navigate)
 
-        self.map = np.zeros((1024, 1024), dtype=np.uint8)
+        self.map = np.zeros((256, 256), dtype=np.int8)
+        self.map_free = np.zeros(self.map.shape, dtype=np.int8)
+        self.map_wall = np.zeros(self.map.shape, dtype=np.int8)
+        self.map_origin = Point(
+            x=-self.map.shape[0] / 2.0 * self.map_resolution,
+            y=-self.map.shape[1] / 2.0 * self.map_resolution,
+        )
 
     def handle_pose(self, t):
         self.tf_broadcaster.sendTransform(t.transforms)
@@ -88,12 +96,22 @@ class Robot(Node):
             sensor_pose = self.tf_buffer.lookup_transform(
                 self.world_frame, self.lidar_frame, rclpy.time.Time()
             )
-            print(sensor_pose)
         except TransformException as ex:
             self.get_logger().info(
                 f'Could not transform "{self.world_frame}" to "{self.lidar_frame}": {ex}'
             )
             return
+
+        robot_map_pose = (
+            round(
+                (sensor_pose.transform.translation.x - self.map_origin.x)
+                / self.map_resolution
+            ),
+            round(
+                (sensor_pose.transform.translation.y - self.map_origin.y)
+                / self.map_resolution
+            ),
+        )
 
         for i, angle in enumerate(
             np.linspace(msg.angle_min, msg.angle_max, len(msg.ranges))
@@ -102,14 +120,33 @@ class Robot(Node):
             if np.isinf(range) or np.isnan(range):
                 range = msg.range_max
 
+            x = np.cos(angle) * range
+            y = np.sin(angle) * range
+            laser_hit_pose = do_transform_pose(
+                Pose(position=Point(x=x, y=y)),
+                TransformStamped(transform=sensor_pose.transform),
+            ).position
+            lidar_hit_map_pose = (
+                round((laser_hit_pose.x - self.map_origin.x) / self.map_resolution),
+                round((laser_hit_pose.y - self.map_origin.y) / self.map_resolution),
+            )
+            cv2.line(self.map_free, robot_map_pose, lidar_hit_map_pose, 1)
+            if range < msg.range_max:
+                cv2.circle(
+                    self.map_wall, lidar_hit_map_pose, radius=1, color=1, thickness=1
+                )
+            self.map[self.map_free == 1] = 127
+            self.map[self.map_wall == 1] = -128
+
             if range < msg.range_max:
                 color = ColorRGBA(r=0.0, g=1.0, b=0.0, a=1.0)
             else:
                 color = ColorRGBA(r=0.0, g=1.0, b=1.0, a=1.0)
-            self.trace_markers.add_point(p, color)
+            self.trace_markers.add_point(laser_hit_pose, color)
 
         self.trace_markers.publish()
         self.publish_map()
+        self.find_path(robot_map_pose, (robot_map_pose[0], robot_map_pose[1] + 100))
 
     def navigate(self):
         pass
@@ -121,10 +158,92 @@ class Robot(Node):
         grid.info = MapMetaData()
         grid.info.height = self.map.shape[0]
         grid.info.width = self.map.shape[1]
-        grid.info.origin.position.x = -grid.info.width / 2.0 * self.map_resolution
-        grid.info.origin.position.y = -grid.info.height / 2.0 * self.map_resolution
+        grid.info.origin.position = self.map_origin
         grid.info.resolution = self.map_resolution
         self.publisher_map.publish(grid)
+
+    def find_path(
+        self, start: tuple[int, int], end: tuple[int, int], max_iterations=10000
+    ):
+        print(f"finding path {start} > {end}")
+        discovered = np.full(self.map.shape, 0.0)
+        parent_map = np.full((self.map.shape[0], self.map.shape[1], 2), -1)
+        score_map = np.full(self.map.shape, np.inf)
+
+        def score(xy):
+            print(f"score {xy}")
+            x, y = xy
+            ways = [
+                (-1, 0),
+                (0, -1),
+                (1, 0),
+                (0, 1),
+                (-1, -1),
+                (1, -1),
+                (1, 1),
+                (-1, 1),
+            ]
+            step_costs = [1, 1, 1, 1, 1.414, 1.414, 1.414, 1.414]
+            for step_cost, (sx, sy) in zip(step_costs, ways):
+                pos = np.array([x + sx, y + sy])
+                # avoid obstacles
+                if discovered[pos[0],pos[1]] or self.map[pos[0], pos[1]] < 0:
+                    print(f"Obstacle at {pos}")
+                    continue
+                dist_cost = np.linalg.norm(pos - end)
+                cost = step_cost + dist_cost
+                if cost < score_map[pos[0], pos[1]]:
+                    score_map[pos[0], pos[1]] = cost
+                    parent_map[pos[0], pos[1]] = [x, y]
+                    if (parent_map[x,y][0] == pos[0] and parent_map[x,y][1] == pos[1]):
+                        raise Exception(f"Circular parenting {[x,y]}<>{pos}")
+
+            discovered[x, y] = True
+
+        score(start)
+
+        while True:
+            next = np.unravel_index(
+                (score_map + discovered * 999999999).argmin(), score_map.shape
+            )
+            # found a path the goal
+            if next[0] == end[0] and next[1] == end[1]:
+                break
+            score(next)
+
+        path = [end]
+        fuse = 1000
+        while True:
+            fuse -= 1
+            if fuse < 0:
+                raise Exception("Failed to build path")
+
+            prev = path[0]
+            parent = parent_map[prev[0], prev[1]]
+            path.insert(0, parent)
+            print(f"building path {path[0]}")
+
+            if parent[0] == start[0] and parent[1] == start[1]:
+                break
+
+        print("PATH", path)
+
+        path_msg = Path()
+        path_msg.header.frame_id = self.world_frame
+        path_msg.poses = [
+            PoseStamped(
+                pose=Pose(
+                    position=Point(
+                        x=pos[0] * self.map_resolution + self.map_origin.x,
+                        y=pos[1] * self.map_resolution + self.map_origin.y,
+                    )
+                )
+            )
+            for pos in path
+        ]
+        self.publisher_path.publish(path_msg)
+
+        
 
 
 # TODO integrate into the robot class
