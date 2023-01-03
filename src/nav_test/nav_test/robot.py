69a8f8520bf4a22afb4efc91e04345e6075f37cd
Polgár András <azazdeaz@gmail.com>
2023-01-03 18:01:54 +0100

    Fix navigation script

--- src/nav_test/nav_test/robot.py
+++ src/nav_test/nav_test/robot.py
@@ -11,6 +11,7 @@ import cv2
 
 import rclpy
 import rclpy.time
+from rclpy import qos
 from rclpy.node import Node
 
 import tf_transformations
@@ -68,21 +69,17 @@ class Robot(Node):
         self.tf_broadcaster = TransformBroadcaster(self)
         self.tf_static_broadcaster = StaticTransformBroadcaster(self)
 
-        # self.subscription_pose = self.create_subscription(
-        #     TFMessage, f"/pose_odom", self.handle_pose, 1
-        # )
         self.subscription_pose_static = self.create_subscription(
-            TFMessage, f"/pose_static", self.handle_pose_static, 1
+            TFMessage, f"/pose_static", self.handle_pose_static, 10
         )
         self.subscription_pose_static = self.create_subscription(
-            LaserScan, f"/scan", self.handle_scan, 1
+            LaserScan, f"/scan", self.handle_scan, 10
         )
         self.publisher_cmd_vel = self.create_publisher(Twist, "cmd_vel", 1)
         self.publisher_map = self.create_publisher(OccupancyGrid, "map", 1)
+        self.publisher_score_map = self.create_publisher(OccupancyGrid, "score_map", 1)
         self.publisher_path = self.create_publisher(Path, "path", 1)
-        self.publisher_goal = self.create_publisher(
-            PoseStamped, "goal", 1
-        )  # TODO publish goal
+        self.publisher_goal = self.create_publisher(PoseStamped, "goal", 1)
         self.publisher_local_goal = self.create_publisher(PoseStamped, "local_goal", 1)
 
         # Calculate and send new navigation commands periodically
@@ -142,24 +139,28 @@ class Robot(Node):
             lidar_hit_map_pose = self.world_pose_to_map(laser_hit_pose)
             cv2.line(map_free, robot_map_pose, lidar_hit_map_pose, 1)
             if range < msg.range_max:
-                cv2.circle(map_wall, lidar_hit_map_pose, radius=1, color=1, thickness=3)
-            self.map[(map_free == 1) & (map_wall == 0) & (self.map < 100)] += 1
-            self.map[(map_wall == 1) & (self.map > -100)] -= -12
+                cv2.circle(map_wall, lidar_hit_map_pose, radius=1, color=1, thickness=5)
+            self.map[(map_free == 1) & (map_wall == 0) & (self.map < 120)] += 1
+            self.map[(map_wall == 1) & (self.map > -110)] -= -12
 
         self.publish_map()
         self.find_path(robot_map_pose, self.world_pose_to_map(self.goal.pose.position))
         self.navigate()
         self.check_goal_reached()
 
-    def publish_map(self):
+    def create_occupancy_grid_msg(self, map):
         grid = OccupancyGrid()
-        grid.data = self.map.ravel().data
+        grid.data = map.ravel().data
         grid.header.frame_id = self.world_frame
         grid.info = MapMetaData()
-        grid.info.height = self.map.shape[0]
-        grid.info.width = self.map.shape[1]
+        grid.info.height = map.shape[0]
+        grid.info.width = map.shape[1]
         grid.info.origin.position = self.map_origin
         grid.info.resolution = self.map_resolution
+        return grid
+
+    def publish_map(self):
+        grid = self.create_occupancy_grid_msg(self.map)
         self.publisher_map.publish(grid)
 
     def save_map_to_image(self):
@@ -170,14 +171,16 @@ class Robot(Node):
         map_img[:, :, 2] = (np.maximum(np.minimum(self.map, 0), -127) * -1).view(
             np.uint8
         ) * 2
-        cv2.imwrite('/tmp/artefacts_output/map.png', map_img)
+        cv2.imwrite("/tmp/artefacts_output/map.png", map_img)
 
-    def find_path(
-        self, start: tuple[int, int], end: tuple[int, int], max_iterations=10000
-    ):
+    # minimal a-start path finder
+    def find_path(self, start: tuple[int, int], end: tuple[int, int]):
+        # flag the computer nodes
         discovered = np.full(self.map.shape, 0.0)
+        # position of the best parent node
         parent_map = np.full((*self.map.shape, 2), -1)
-        score_map = np.full(self.map.shape, np.inf)
+        # the g-cost and h-cost of each node
+        score_map = np.full((*self.map.shape, 2), np.inf)
 
         # discover one node, and update its neighbours
         def score(xy):
@@ -194,6 +197,8 @@ class Robot(Node):
             ]
             step_costs = [1, 1, 1, 1, 1.414, 1.414, 1.414, 1.414]
 
+            parent_g_cost = score_map[x, y][0]
+
             for step_cost, (sx, sy) in zip(step_costs, ways):
                 pos = np.array([x + sx, y + sy])
                 # skip out of map nodes
@@ -205,12 +210,18 @@ class Robot(Node):
                 ):
                     continue
                 # skip descovered nodes and obstacles
-                if discovered[pos[0], pos[1]] or self.map[pos[0], pos[1]] < 0:
+                if discovered[pos[0], pos[1]]:
                     continue
+                # self.map is in row-major (y, x indexed)
+                if self.map[pos[1], pos[0]] < 0:
+                    danger_cost = 1000.0
+                else:
+                    danger_cost = 0.0
                 dist_cost = np.linalg.norm(pos - end)
-                cost = step_cost + dist_cost
-                if cost < score_map[pos[0], pos[1]]:
-                    score_map[pos[0], pos[1]] = cost
+                g_cost = parent_g_cost + step_cost
+                h_cost =  danger_cost + dist_cost
+                if g_cost + h_cost < score_map[pos[0], pos[1]].sum():
+                    score_map[pos[0], pos[1]] = [g_cost, h_cost]
                     parent_map[pos[0], pos[1]] = [x, y]
                     if parent_map[x, y][0] == pos[0] and parent_map[x, y][1] == pos[1]:
                         raise Exception(f"Circular parenting {[x,y]}<>{pos}")
@@ -218,13 +229,15 @@ class Robot(Node):
             discovered[x, y] = True
 
         # discover the first node (current pose of the robot)
+        score_map[start[0], start[1], :] = 0
         score(start)
 
-        # TODO should we use recursive functions instead of while loops?
         # keep discovering nodes until the end-node is reached
         while not np.all(discovered):
+            # select an undiscovered node with the lowest score
             next = np.unravel_index(
-                (score_map + discovered * 999999999).argmin(), score_map.shape
+                (score_map.sum(axis=2) + discovered * 999999999).argmin(),
+                (score_map.shape[0], score_map.shape[1]),
             )
             # found a path to the goal
             if next[0] == end[0] and next[1] == end[1]:
@@ -241,6 +254,7 @@ class Robot(Node):
                 return
 
             prev = path[0]
+            self.get_logger().info(f"Score trail {prev}: {score_map[prev[0], prev[1]]}")
             parent = parent_map[prev[0], prev[1]]
             path.insert(0, parent)
 
@@ -261,6 +275,17 @@ class Robot(Node):
             )
             for pos in path
         ]
+
+        # Publish the score map for debugging
+        score_map = np.transpose(score_map.sum(axis=2))
+        min_score = score_map.min()
+        max_score = score_map[score_map != np.inf].max()
+        int_scores = (score_map - min_score) / (max_score - min_score) * -255 + 127
+        for xy in path:
+            int_scores[xy[0], xy[1]] = 0
+        grid = self.create_occupancy_grid_msg(int_scores.astype(np.int8))
+        self.publisher_score_map.publish(grid)
+
         self.publisher_path.publish(self.path)
 
     def lookup_transform(self, target_frame: str, source_frame: str):
