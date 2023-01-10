7313ba4b301a5c34edd1081ae1c97d8085b6bff0
Polgár András <azazdeaz@gmail.com>
2023-01-10 10:04:34 +0100

    wip-params

--- src/nav_test/test/test_navigation.launch.py
+++ src/nav_test/test/test_navigation.launch.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import yaml
 from ament_index_python.packages import get_package_share_directory
 import pytest
 from launch_testing.actions import ReadyToTest
@@ -8,6 +9,21 @@ from launch.actions import ExecuteProcess, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
+PARAMS_FILE = '/tmp/scenario_params.yaml'
+
+def read_params():
+    try:
+        with open(PARAMS_FILE, "r") as f:
+            configuration = yaml.safe_load(f)
+            print(f"Loaded configuration: {configuration}")
+            print(f'Extrinsics: {configuration["test"]["ros__parameters"]["seed"]}')
+            # TODO handle missing values
+            seed = configuration["test"]["ros__parameters"]["seed"]
+            distance = configuration["test"]["ros__parameters"]["distance"]
+            return seed, distance
+    except FileExistsError as e:
+        print(f"Failed to read '{PARAMS_FILE}': {e}")
+        return 0, 6
 
 @pytest.mark.launch_test
 def generate_test_description():
@@ -15,7 +31,7 @@ def generate_test_description():
     bag_recorder = ExecuteProcess(
         cmd=rosbag_cmd, output="screen", additional_env={"PYTHONUNBUFFERED": "1"}
     )
-
+    seed, distance = read_params()
     # snapshot sky
     snapshot_sky = Node(
         package="nav_test",
@@ -56,7 +72,7 @@ def generate_test_description():
             ]
         ),
         launch_arguments={
-            # "seed": "7",
+            "seed": seed,
         }.items(),
     )
 
